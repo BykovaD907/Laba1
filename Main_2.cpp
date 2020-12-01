ad9ccc1e71785ef7393f78b1f6e1bf76643d2f07
Bykova Daria <74949638+BykovaD907@users.noreply.github.com>
2020-12-01 09:32:07 +0700

    Add files via upload

--- Main_2.cpp
+++ Main_2.cpp
@@ -5,59 +5,14 @@
 #include <ctime>
 #include "Sq_Matrix.h"
 using namespace std;
-
-void Menu_matrix(int &ex)
-{
-	int ch, x;
-	system("cls");
-	cout << "1. Транспонирование матрицы\n2. Умножение матрицы на число\n3. Сложение мматриц\n0. Выход\n\nВведите номер команды: ";
-	cin >> ch;
-	system("cls");
-
-	switch (ch)
-	{
-	case 0: {ex = 1; } break;
-	case 1: 
-	{
-		cout << "Введите число строк / столбцов в квадратной матрице: ";
-		cin >> x;
-		Sq_Matrix a(x);
-		a.Transp_Mstrix();
-		_getch(); 
-	}	break;
-	case 2: 
-	{
-		cout << "Введите число строк / столбцов в квадратной матрице: ";
-		cin >> x;
-		Sq_Matrix a(x);
-		a.Multipl_number();
-		_getch(); 
-	}	break;
-	case 3: 
-	{
-		cout << "Введите число строк / столбцов в квадратной матрице: ";
-		cin >> x;
-		Sq_Matrix a(x);
-		a.Matrix_add();
-		_getch(); 
-	}	break;
-	default:
-	{	
-		cout << "Несуществующий пункт. Для продолжения нажмите любую клавишу.";
-		_getch();
-		Menu_matrix(ex); 
-	}	break;
-	}
-}
-
-
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(NULL));
-
-	int ex = 0;
-	while (ex == 0)  Menu_matrix(ex);
-	return 0;
+	int x;
+	cout << "enter the dimension of the matrix" << endl;
+	cin >> x;
+	Sq_Matrix a(x);
+	a.Transp_Mstrix();
+	a.Multipl_number();
+	a.Matrix_add();
+	exit(0);
 }
