4ee8cf52965313dfab07be9db27b08a38ed4b1aa
Bykova Daria <74949638+BykovaD907@users.noreply.github.com>
2020-12-08 20:38:38 +0700

    Add files via upload

--- Sq_Matrix.cpp
+++ Sq_Matrix.cpp
@@ -39,4 +39,44 @@ void Sq_Matrix::Transp_Mstrix()
 		}
 	}
 }
+Sq_Matrix* operator+ (Sq_Matrix& Str)
+{
+	Sq_Matrix* Temp = new Sq_Matrix(this->x);
+	for (int i = 0; i < Str.getX(); i++)
+	{
+		for (int j = 0; j < Str.getX(); j++)
+		{
+			Temp->matrix[i][j] = Str.matrix[i][j] + matrix[i][j];
+		}
+	}
+	return Temp;
+}
+inline friend ostream& operator<< (ostream& Str, Sq_Matrix& v)
+{
+	Str << "matrix: ";
+	int element = 0;
 
+	for (int i = 0; i < v.getX(); i++)
+	{
+		Str.put('\n');
+		for (int j = 0; j < v.getX(); j++)
+		{
+			element = v.getMatrix();
+			Str.put(' ');
+			Str << element;
+		}
+	}
+	return Str;
+}
+Sq_Matrix* operator*(const int& Multiplier)
+{
+	Sq_Matrix* Temp = new Sq_Matrix(x);
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			Temp->matrix[i][j] = this->matrix[i][j] * Multiplier;
+		}
+	}
+	return Temp;
+}
--- Sq_Matrix.h
+++ Sq_Matrix.h
@@ -23,29 +23,6 @@ public:
 };
 class Sq_Matrix
 {
-
-	inline friend ostream& operator<< (ostream& Str, Sq_Matrix& v)
-	{
-		Str << "matrix: ";
-		int element = 0;
-
-		for (int i = 0; i < v.getX(); i++)
-		{
-			Str.put('\n');
-			for (int j = 0; j < v.getX(); j++)
-			{
-				element = v.getMatrix()[i][j];
-				Str.put(' ');
-				Str << element;
-			}
-		}
-		return Str;
-	}
-
-private:
-	int x;// порядок матрицы (размерность)
-	int** matrix;
-
 public:
 	Sq_Matrix(Sq_Matrix& o)
 		:x(o.getX()), matrix(o.getMatrix())
@@ -64,32 +41,13 @@ public:
 	}
 	inline int** getMatrix()
 	{
-		return matrix;
+		return matrix[i][j];
 	}
 	
-	Sq_Matrix* operator+ (Sq_Matrix& Str)
-	{
-		Sq_Matrix* Temp = new Sq_Matrix(this->x);
-		for (int i = 0; i < Str.getX(); i++)
-		{
-			for (int j = 0; j < Str.getX(); j++)
-			{
-				Temp->matrix[i][j] = Str.matrix[i][j] + matrix[i][j];
-			}
-		}
-		return Temp;
-	}
-	Sq_Matrix* operator*(const int& Multiplier)
-	{
-		Sq_Matrix* Temp = new Sq_Matrix(x);
-		for (int i = 0; i < x; i++)
-		{
-			for (int j = 0; j < x; j++)
-			{
-				Temp->matrix[i][j] = this->matrix[i][j] * Multiplier;
-			}
-		}
-		return Temp;
-	}
 	void Transp_Mstrix();
+
+private:
+	int x;// порядок матрицы (размерность)
+	int** matrix;
+
 };
